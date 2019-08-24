c8bf76ccc2cce725ed1f3a9da9694176376f69b3
Zebbeni <andrew.albers@gmail.com>
2019-08-24 17:02:37 -0600

    create ChatColumn class

--- flutter_web_client/lib/main.dart
+++ flutter_web_client/lib/main.dart
@@ -39,29 +39,56 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            RawKeyboardListener(
-              focusNode: FocusNode(),
-              onKey: (event) {
-                // todo: We ought to be able to use _sendMessage as the TextField's
-                // onSubmitted callback, but this isn't working for web right now.
-                // https://github.com/flutter/flutter/issues/19027
-                bool isRawKeyEvent = event.runtimeType == RawKeyDownEvent;
-                bool isEnter = event?.logicalKey?.keyId == 54;
-                if (isRawKeyEvent && isEnter) {
-                  _sendMessage();
-                }
-              },
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(labelText: 'type a message and hit enter'),
-              ),
-            ),
-          ],
+        child: ChatColumn(
+          channel: widget.channel,
         ),
       ),
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+}
+
+class ChatColumn extends StatefulWidget {
+  final TextEditingController controller = TextEditingController();
+  final HtmlWebSocketChannel channel;
+
+  ChatColumn({Key key, @required this.channel}) : super(key: key);
+
+  @override
+  ChatColumnState createState() => ChatColumnState();
+}
+
+class ChatColumnState extends State<ChatColumn> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        RawKeyboardListener(
+          focusNode: FocusNode(),
+          onKey: (event) {
+            // todo: We ought to be able to use _sendMessage as the TextField's
+            // onSubmitted callback, but this isn't working for web right now.
+            // https://github.com/flutter/flutter/issues/19027
+            bool isRawKeyEvent = event.runtimeType == RawKeyDownEvent;
+            bool isEnter = event?.logicalKey?.keyId == 54;
+            if (isRawKeyEvent && isEnter) {
+              _sendMessage();
+            }
+          },
+          child: TextField(
+            controller: _controller,
+            decoration: InputDecoration(labelText: 'type a message and hit enter'),
+          ),
+        ),
+      ],
     );
   }
 
@@ -70,11 +97,5 @@ class _MyHomePageState extends State<MyHomePage> {
     if (_controller.text.isNotEmpty) {
       widget.channel.sink.add(_controller.text);
     }
-  }
-
-  @override
-  void dispose() {
-    widget.channel.sink.close();
-    super.dispose();
   }
 }
